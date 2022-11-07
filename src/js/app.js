183c94653a1e8fd67456d36ec9a153175efa136c
Maxim <ventilyatortag@gmail.com>
2022-11-07 17:51:04 +0300

    Main javascript file

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+import '../index.ejs';
+import '../scss/style.scss';
+import { sliderNumber } from './plugins/custom-slider';
+
+const preview = document.querySelector('.preview');
+
+sliderNumber('#number')
